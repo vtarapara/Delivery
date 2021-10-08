2956682d7c077525d2b781f7f37854f0aeb816d3
kdk1616 <kenneth.kalin@duke.edu>
2021-10-08 12:52:11 -0400

    Created Calendar to visualize inputs from form

--- Website/homepage.php
+++ Website/homepage.php
@@ -1,16 +1,28 @@
 <?php
+include 'Calendar.php';
 
 $row = 1;
+$calendar = new Calendar('2021-9');
+
+echo "<h1> Calendar </h1>"; 
 if (($handle = fopen("data.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-      $num = count($data);
-      echo "<p> $num fields in line $row: <br /></p>\n";
-      $row++;
-      for ($c=0; $c < $num; $c++) {
-       echo $data[$c] . "<br />\n";
-   }
+    $num = count($data);
+	   $calendar->add_event($data[0] . " -- " . $data[1], $data[2]);	   
+   
  }
+ 
+ 
+ 
+ echo $calendar;
+ 
+ 
  fclose($handle);
 }
 
 ?>
+
+<html>
+
+  <link rel="stylesheet" href="calendar.css">
+  </html>
