987d5ca887e2ded5dbea108ce8528e92dc6a1b5d
kdk1616 <kenneth.kalin@duke.edu>
2021-10-11 11:21:22 -0400

    Changes from weekend, added ability to select viewed month

--- Website/form.php
+++ Website/form.php
@@ -1,45 +1,51 @@
 <?php
 
 
-
 if (isset($_POST['submit'])) {
 	
 	$name = $_POST['name'];
 	$descrip = $_POST['descrip'];
 	$date = $_POST['date'];
+	$time = $_POST['date'];
 	
+	$toAdd = array($name,$descrip,$date,$time);
+	
+	$dupe = FALSE;
+	if (($handle = fopen("data.csv", "r")) !== FALSE) {
+	    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+			if($data == $toAdd) {
+				$dupe = TRUE;
+			}
+			
+		}
+	}
+	
+	if(!$dupe) {
 	$fp = fopen('data.csv', 'a+');
-	fputcsv($fp, array($name,$descrip,$date));
+	fputcsv($fp, $toAdd);
 	fclose($fp);
-
+	}
+	else {
+		echo "DID NOT ADD ITEM SINCE DUPLICATE ALREADY EXISTS";
+	}
 
 }
-
-
-
-
-
-
-
 
 ?>
 
 
 <html>
+  <link rel="stylesheet" href="form.css">
 
+<h1> Schedule New Delivery: </h1>
 <form method="post" name="form" enctype="multipart/form-data">
     <input type="text" name="name" id="name" placeholder="Company Name" required/>
     <input type="text" name="descrip" id="descrip" placeholder="Delivery Description" required/>
-    <input type="text" name="date" id="date" placeholder="Delivery Date" required/>
+    <input type="date" name="date" id="date" placeholder="Delivery Date" required/>
+    <input type="time" name="time" id="time" required/>
+	
     <input type="submit" name="submit" value="Send" onclick="myFunction()"/>
 </form>
-
-
-<script>
-	
-	
-</script>
-
 
 
 
--- Website/homepage.php
+++ Website/homepage.php
@@ -2,7 +2,16 @@
 include 'Calendar.php';
 
 $row = 1;
-$calendar = new Calendar('2021-9');
+$transdate = date('m-d-Y', time());
+$month = date('m', strtotime($transdate));
+
+if (isset($_POST['month'])) {
+	$calendar = new Calendar($_POST['month']);
+	$month = $_POST['month'];
+}
+else {
+	$calendar = new Calendar($month);
+}
 
 echo "<h1> Calendar </h1>"; 
 if (($handle = fopen("data.csv", "r")) !== FALSE) {
@@ -14,17 +23,29 @@ if (($handle = fopen("data.csv", "r")) !== FALSE) {
  
  
  
+ 
+ 
+ 
+ echo "<form method='POST' action='homepage.php'>";
+ echo "<input type='month' id='start' name='month' value='".$month."'>";
+ echo "<input type='submit' value='Change Month'>";
+ echo "</form>";
  echo $calendar;
  
  
  fclose($handle);
 }
 
+
+
+
 ?>
 
 <html>
 
   <link rel="stylesheet" href="calendar.css">
+  
+  
   
   <script>
 	  function popup() {
