7474ff22d4adfecdce926d4c88dd7fe9f629959f
kdk1616 <kenneth.kalin@duke.edu>
2021-10-29 12:07:00 -0400

    Wrote calExport.php and dayExport.php to export cal data as JSON

--- Website/dayExport.php
+++ Website/dayExport.php
@@ -5,6 +5,8 @@ $file_name = "data.csv"; // My file name is 1_23.csv
 $file_path = "./".$file_name;
 $file_handle = fopen($file_path, "r");
 
+$dateString = $_GET['date'];
+
 $result = array();
 
 if ($file_handle !== FALSE) {
@@ -13,6 +15,8 @@ $columns = array("Name","Descrip","Date","Time");
 
     while (($data = fgetcsv($file_handle)) !== FALSE) {
 
+
+if(strcmp($data[2], $dateString) == 0) {
 $arr = array($columns[0] => $data[0], 
             $columns[1] => $data[1], 
              $columns[2] => $data[2], 
@@ -20,14 +24,14 @@ $arr = array($columns[0] => $data[0],
 
 
 	array_push($result,array($data[2] => $arr));
-    }
+	}    
+}
 
 
     fclose($file_handle);
 }
 
 // print_r($result); // I see all data(s) except the header
-
 
 $json = json_encode($result);
 $json = trim($json, '[]');
